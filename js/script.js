08c564df059b280c70f7013b71ce7a1fbe96933f
pranav thakur <70392540+pranavthakur404@users.noreply.github.com>
2021-12-09 22:16:18 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// video btn
+let videoBtn = document.getElementById('video-btn');
+
+videoBtn.addEventListener('click', function(){
+    let videoContent = document.getElementById('video-ctn').style.display='flex';
+});
